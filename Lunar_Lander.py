b57a3c5c8459d5b4136fcb9865c9b76eeb1a4e0f
bcao6124 <bcao6124@apps4pps.net>
2018-05-31 20:39:21 +0000

    Finished making functions for the game, some improvements may be added

--- Lunar_Lander.py
+++ Lunar_Lander.py
@@ -6,9 +6,59 @@ main_thruster=Button(13)
 right_thruster=Button(19)
 lose=LED(20)
 win=LED(21)
+right_light=LED(26)
+middle_light=LED(22)
+left_light=LED(27)
 
-while True:
-    left.thruster
+def liftoff ():
+    if main_thruster.when_pressed:
+        middle_light.on
+        sleep(1)
+        middle_light.off
+        return (True)
+    else:
+        return(False)
+
+def left ():
+    if left_thruster.when_pressed:
+        left_light.on
+        sleep(1)
+        left_light.off
+        return (True)
+    else:
+        return(False)
+
+def right ():
+    if right_thruster.when_pressed:
+        right_light.on
+        sleep(1)
+        right_light.off
+        return (True)
+    else:
+        return(False)
+def Lose ():
+    lose.on
+    sleep(3)
+    lose.off
+
+def Win ():
+    win.on
+    sleep(3)
+    win.off
+    
+    
+
+
+    
+    
+        
+        
+        
+            
+            
+        
+        
+    
     
 
 
